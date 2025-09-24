11f6394e1e59361c6c773aba731d37cb8bbac8f6
Ameen <shehaaa4@gmail.com>
2025-09-24 10:15:10 +0300

    .

--- src/Pages/Home.vue
+++ src/Pages/Home.vue
@@ -1,5 +1,15 @@
 <template>
-  <div></div>
+  <div class="p-8 py-4 h-[100vh] rounded-[40px] border-t border-b">
+    <div class="flex">
+      <div class="w-[15%]">
+        <div class="logo gap-1 flex items-center">
+          <img class="w-[60px]" src="../assets/logo.jpg" alt="" />
+          <p class="fruktur-regular text-[20px]">CHATBOT</p>
+        </div>
+      </div>
+      <div class="w-[85%]">s</div>
+    </div>
+  </div>
 </template>
 
 <script>
